7aea6bf77dac8a6cd27c0077bc540ba87e9c5fd9
Ara Bela Zulfa Laila <143501269+arabelazulfa@users.noreply.github.com>
2025-07-07 18:25:39 +0700

    Update1 app.py

--- easyocr_app/app.py
+++ easyocr_app/app.py
@@ -5,6 +5,8 @@ import numpy as np
 import streamlit.components.v1 as components
 
 ga_id = st.secrets["GA_ID"]
+
+# Sisipkan Google Analytics Tracking Script
 ga_code = f"""
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id={ga_id}"></script>
@@ -24,9 +26,10 @@ st.title("OCR Dokumen Bahasa Indonesia dengan EasyOCR")
 uploaded_file = st.file_uploader("Upload gambar dokumen", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Gambar yang di-upload", use_column_width=True)
+    st.image(image, caption="Gambar yang di-upload", use_container_width=True)
 
     reader = easyocr.Reader(['id'])
     result = reader.readtext(np.array(image), detail=0)
+
     st.subheader("Hasil OCR:")
     st.write(" ".join(result))
