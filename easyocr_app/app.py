b26308bad103b2aeb592c1259f8f1149828641c2
Ara Bela Zulfa Laila <143501269+arabelazulfa@users.noreply.github.com>
2025-07-07 21:07:52 +0700

    Update app.py

--- easyocr_app/app.py
+++ easyocr_app/app.py
@@ -9,19 +9,20 @@ import os
 import io
 
 gtm_id = st.secrets["GTM_ID"]
-gtm_code = """
+gtm_code = f"""
 <!-- Google Tag Manager -->
 <script>
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id=GTM-W9GZB9ZN'+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W9GZB9ZN');
+(function(w,d,s,l,i){{w[l]=w[l]||[];w[l].push({{'gtm.start':
+new Date().getTime(),event:'gtm.js'}});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
+j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+f.parentNode.insertBefore(j,f);}})(window,document,'script','dataLayer','{gtm_id}');
 </script>
-<!-- End Google Tag Manager -->
+
+<!-- Google Tag Manager (noscript) -->
+<iframe src="https://www.googletagmanager.com/ns.html?id={gtm_id}"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
 """
-
-
 components.html(gtm_code, height=100, width=300)
 
 st.title("OCR Dokumen Bahasa Indonesia dengan EasyOCR")
